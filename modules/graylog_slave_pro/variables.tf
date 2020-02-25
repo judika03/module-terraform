fcec0a67eed86b93b498ea929e0489c7066fbeab
System Administrator <root@ID-AFRITAROSAVIANTI.local>
2020-02-25 16:57:34 +0700

    adding module graylog pro

---
+++ modules/graylog_slave_pro/variables.tf
@@ -0,0 +1,71 @@
+
+
+variable "project_id" {
+  description = "The GCP project to use for integration tests"
+}
+
+variable "region" {
+  description = "The GCP region to create and test resources in"
+  type        = string
+  default     = "asia-southeast1"
+}
+
+variable "private_key" {
+  default = "~/.ssh/MyKeyPair.pem"
+}
+
+variable "zone" {
+  description = "The GCP region to create and test resources in"
+  type        = string
+  default     = "asia-southeast1-a"
+}
+variable "network" {
+  description = "The subnetwork to host the compute instances in"
+  default     = "default"
+}
+
+variable "subnetwork" {
+  description = "The subnetwork to host the compute instances in"
+  default     = "default"
+}
+
+variable "subnetwork_project" {
+  description = "The subnetwork to host the compute instances in"
+  default     = "spid-prod"
+}
+variable "num_instances" {
+  description = "Number of instances to create"
+  default="1"
+}
+variable "machine_type" {
+
+}
+
+variable "tags" {
+  description = "Number of instances to create"
+  default="teser"
+}
+
+variable "ame-firewall" {
+  description = "Number of instances to create"
+  default="teser"
+}
+
+
+variable "ansible_user" {
+  description = "Ansible user that will be used to authenticate the SSH"
+  type = string
+  default = "judika.gultom"
+}
+variable "project_id_image" {
+
+}
+
+variable "project_id_nodes" {
+
+}
+
+
+variable "project_id_address" {
+
+}
